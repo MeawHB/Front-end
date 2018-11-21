6ba5ec85101ddd737c792a3c3e71fc4a37b2f724
wlmiag <39036193@qq.com>
2018-11-21 08:58:21 +0800

    2

--- 小案例/资源管理器/test/imagestest.js
+++ 小案例/资源管理器/test/imagestest.js
@@ -2,7 +2,7 @@ var images = require("images");
 
 images("C:\\Users\\jf\\Desktop\\tupianceshi\\111.jpg")                     //Load image from file
 //加载图像文件
-    .size(400)                          //Geometric scaling the image to 400 pixels width
+    .size(1000)                          //Geometric scaling the image to 400 pixels width
     //等比缩放图像到400像素宽
     // .draw(images("logo.png"), 10, 10)   //Drawn logo at coordinates (10,10)
     //在(10,10)处绘制Logo
