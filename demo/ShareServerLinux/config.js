9b12c43d535ab4bd4ed29bace8aa0f577789a2f1
wlmiag <39036193@qq.com>
2018-11-23 15:14:02 +0800

    2

--- demo/ShareServerLinux/config.js
+++ demo/ShareServerLinux/config.js
@@ -1,5 +1,10 @@
+// module.exports = {
+//     "port": 3000,
+//     "directory": "/home/keyide/Pictures",
+// };
+
+
 module.exports = {
     "port": 3000,
-    "directory": "/home/keyide/Pictures",
-    "simg": "/home/keyide/Pictures"
+    "directory": "C:\\Users\\jf\\Desktop\\gx",
 };
