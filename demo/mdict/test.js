90f3d7746247c3bdfbdf4431b20136f6a8ec3594
wlmiag <39036193@qq.com>
2018-12-28 11:25:30 +0800

    2

--- demo/mdict/test.js
+++ demo/mdict/test.js
@@ -1,6 +1,21 @@
-import Mdict from "js-mdict";
+var mdict = require('mdict');
 
-const mdict = new Mdict("mdx/oale8.mdx");
-console.log(mdict.lookup("hello"));
-console.log(mdict.prefix("hello"));
-test.js;
+mdict.dictionary('./mdx/new_oxford_v2.mdx').then(function (dictionary) {
+    //// dictionary is loaded
+    dictionary.search({
+        phrase: 'plague', /// '*' and '?' supported
+        max: 10	          /// maximum results
+    }).then(function (foundWords) {
+        console.log('Found words:');
+        console.log(foundWords);      /// foundWords is array
+
+        var word = '' + foundWords[0];
+        console.log('Loading definitions for: ' + word);
+        return dictionary.lookup(word); /// typeof word === string
+    }).then(function (definitions) {
+        console.log('definitions:');     /// definition is array
+        console.log(definitions);
+
+    });
+
+});
